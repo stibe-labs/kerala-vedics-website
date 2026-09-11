3df92f3244eca15033e3d71430438f68e557bbb9
ItsAbhishekPH <abhishekkadampuzha292@gmail.com>
2026-09-11 16:59:41 +0530

    Redirect keralavedics.com to https://www.keralavedics.com

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,32 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: '^keralavedics\\.com$',
+          },
+        ],
+        destination: 'https://www.keralavedics.com/',
+        permanent: true,
+      },
+      {
+        source: '/:path+',
+        has: [
+          {
+            type: 'host',
+            value: '^keralavedics\\.com$',
+          },
+        ],
+        destination: 'https://www.keralavedics.com/:path+',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
